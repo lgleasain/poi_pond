01b0606ef12b601c86d821608852c3deaf215bb2
Lance Gleason <lgleasain@yahoo.com>
2011-03-27 21:52:59 -0400

    added image support and beginnig streamlined spreadsheet creation code.

--- lib/poi_pond.rb
+++ lib/poi_pond.rb
@@ -10,11 +10,11 @@ module POIPond
     else
       jardir = File.join('.','javalibs', '**', '*.jar')
     end
-    Rjb::load(classpath = Dir.glob(jardir).join(':'), jvmargs=[])
+    Rjb::load(classpath = Dir.glob(jardir).join(':'), jvmargs=['-Djava.awt.headless=true'])
   end
   
-  def create_excel_workbook
-    Rjb::import('org.apache.poi.hssf.usermodel.HSSFWorkbook').new
+  def create_excel_workbook(file = nil)
+    file ? Rjb::import('org.apache.poi.hssf.usermodel.HSSFWorkbook').new(file) : Rjb::import('org.apache.poi.hssf.usermodel.HSSFWorkbook').new
   end
   
   def create_excel_cell_range_address
@@ -25,7 +25,28 @@ module POIPond
     Rjb::import('org.apache.poi.hssf.usermodel.HSSFDataFormat')
   end
   
-  def poi_file(file)
+  def poi_output_file(file)
     Rjb::import('java.io.FileOutputStream').new(file)
   end
+
+  def poi_input_file(file)
+    Rjb::import('java.io.FileInputStream').new(file)
+  end
+  
+  def add_photo_to_sheet(workbook, sheet, row, column, image)
+    picture_index = workbook.addPicture image, workbook.PICTURE_TYPE_JPEG
+    drawing = workbook.getSheet(sheet).createDrawingPatriarch
+    anchor = workbook.getCreationHelper.createClientAnchor
+    anchor.setCol1 column
+    anchor.setRow1 row
+    drawing.createPicture(anchor, picture_index).resize
+  end
+  
+  def create_spreadsheet(options)
+    workbook = create_excel_workbook
+    options.each do |sheet|
+      workbook.createSheet sheet[:sheet][:name]
+    end
+    workbook
+  end  
 end 
--- test/test_poi_pond.rb
+++ test/test_poi_pond.rb
@@ -14,24 +14,52 @@ class TestPoiPond < Test::Unit::TestCase
     setup do
       initialize_poi
     end
+
+    context "primitive functions" do
+      should "create a workbook" do
+        workbook = create_excel_workbook
+        assert workbook.createSheet('sheet1')
+      end
     
-    should "create a workbook" do
-      workbook = create_excel_workbook
-      assert workbook.createSheet("sheet1")
+      should "create a cell range address object" do
+        assert create_excel_cell_range_address.valueOf('$A$2')
+      end
+    
+      should "create a HSSFDataFormat object" do
+        reference_built_in_format = Rjb::import('org.apache.poi.hssf.usermodel.HSSFDataFormat').getBuiltinFormat("m/d/yy h:mm")
+        assert_equal reference_built_in_format, hssf_data_format.getBuiltinFormat("m/d/yy h:mm")
+      end
+    
+      should "create a FileOutputStream object" do
+        assert_equal Rjb::import('java.io.FileOutputStream').new('foo').java_methods, poi_output_file('foo').java_methods
+      end
+
+      should "create a FileInputStream object" do
+        assert_equal Rjb::import('java.io.FileInputStream').new('foo').java_methods, poi_input_file('foo').java_methods
+      end
+    
+      should "create a image and place it on a worksheet" do
+        test_image = File.join(File.dirname(File.dirname(__FILE__)), 'test', 'image001.jpg')
+        workbook = create_excel_workbook
+        workbook.createSheet('sheet1')
+        add_photo_to_sheet(workbook, 'sheet1', 1, 1, File.new(test_image).bytes.to_a)
+        workbook.write(poi_output_file('my_test.xls'))
+        read_workbook = create_excel_workbook poi_input_file('my_test.xls')
+        assert_equal File.new(test_image).bytes.to_a, read_workbook.getAllPictures.get(0).getData.bytes.to_a
+        File.delete 'my_test.xls'
+      end        
     end
     
-    should "create a cell range address object" do
-      assert create_excel_cell_range_address.valueOf('$A$2')
-    end
-    
-    should "create a HSSFDataFormat object" do
-      reference_built_in_format = Rjb::import('org.apache.poi.hssf.usermodel.HSSFDataFormat').getBuiltinFormat("m/d/yy h:mm")
-      assert_equal reference_built_in_format, hssf_data_format.getBuiltinFormat("m/d/yy h:mm")
-    end
-    
-    should "create a FileOutputStream object" do
-      assert_equal Rjb::import('java.io.FileOutputStream').new('foo').java_methods, poi_file('foo').java_methods
+    context "create" do
+      should "create a spreadsheet with one a sheet called sheet1" do
+        assert_not_nil create_spreadsheet([:sheet => {:name => 'sheet1'}]).getSheet('sheet1')
+      end
+      
+      should "create a spreadsheet with multiple sheets" do
+        spreadsheet = create_spreadsheet([{:sheet => {:name => 'sheet1'}}, {:sheet => {:name => 'sheet2'}}])
+        assert_not_nil spreadsheet.getSheet('sheet1')
+        assert_not_nil spreadsheet.getSheet('sheet2')
+      end
     end
   end
-  
 end
